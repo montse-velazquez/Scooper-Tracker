b471c62c6dc5a7c5558fb2802639ab0e31d07ec7
montse-velazquez <12337@coderacademy.edu.au>
2022-12-17 07:47:15 +1100

    Added new function to the 'Mathematics' class, for getting the average of the day

--- languages.py
+++ languages.py
@@ -1,6 +1,8 @@
 import os
 from datetime import date
 from statistics import mean
+from math import isnan
+from itertools import filterfalse
 
 
 flavours={"pistacho": 0, "cookies and cream": 0, "chocolate": 0, "vanilla": 0, "strawberry": 0, "dulce de leche": 0, "chocalte chip": 0, "chocolate mint": 0, "mango": 0, "coffee": 0}
@@ -98,7 +100,7 @@ def printing_flavours(flav):
     for flavs in flav:
         print(flavs.capitalize(), "sold -",flav[flavs], "Scoops")
 
-#Add file, for having records of the sales and scoops sold
+# Add file, for having records of the sales and scoops sold
 # f = open("myfile.txt", "a")
 
 # def Upload_file(add_list, which_is):
@@ -112,6 +114,7 @@ def printing_flavours(flav):
 #         f.write(str(add_list[flavs]))
 #         f.write("\n")
 
+#Class where it will be stored with fucntions about stadistics, such as the trend, flavors most time scooped and others.
 class Mathematics: 
     def __init__(self, listes, name):
         self.listes = listes
@@ -131,6 +134,29 @@ class Mathematics:
         else: 
             print("The flavour with bigger selling today was", key_list[position].capitalize(), "by selling: ", sort_lists[-1], "Scoops")
         return sort_lists
+
+    def get_media(self):
+        average = (float(mean(list(self.listes.values()))))
+        sum_up = sum(list(self.listes.values())) 
+        return print("From ", sum_up, "scoops sold of the", self.name, ",at least", average, "of the scoops were of this same flavour"   )
+
+    def get_less_favorite(self):
+        for i in self.listes:
+            if self.listes[i] == 0:
+                self.listes[i] = float('NaN')
+            else: 
+                pass
+        print(sum(map(isnan, list(self.listes.values()))), '/', len(self.listes), "flavours in", self.name, "didnt sold any scoop at all")
+
+
+        clean = list(filterfalse(isnan, list(self.listes.values())))
+        key_list = list(self.listes.keys())
+        val_list = list(self.listes.values())
+        print("The next flavours were the only ones that made sales today")
+        for i in clean: 
+            position = val_list.index(i)
+            print("- ", key_list[position].capitalize())
+
          
 ### MAIN PROGRAM ###
 
@@ -168,8 +194,12 @@ while choice != 'q':
         sort_1 = Mathematics(flavours, "Classic flavours")
         sort_2 = Mathematics(specials, "Special flavours")
         sort_1.sort_list()
+        sort_1.get_media()
+        sort_1.get_less_favorite()
         print('\n')
         sort_2.sort_list()
+        sort_2.get_media()
+        sort_2.get_less_favorite()
     elif choice == 'q':
         print("\nThank you for your order. See you soon!.")
     else:

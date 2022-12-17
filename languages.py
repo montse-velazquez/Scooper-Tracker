f2c5e197ca22a84f2971349a8e81b5215e11a566
montse-velazquez <12337@coderacademy.edu.au>
2022-12-17 13:12:42 +1100

    Create the first test for the feature that makes the sales of the day. Create new file for neext test

--- languages.py
+++ languages.py
@@ -126,47 +126,6 @@ def printing_flavours(flav):
 #         f.write(str(add_list[flavs]))
 #         f.write("\n")
 
-#Class where it will be stored with fucntions about stadistics, such as the trend, flavors most time scooped and others.
-# class Mathematics: 
-#     def __init__(self, listes, name):
-#         self.listes = listes
-#         self.name = name
-    
-#     def sort_list(self):
-#         sort_lists = []
-#         for flavs in self.listes:
-#             sort_lists.append(self.listes[flavs])
-#         sort_lists = sorted(sort_lists)
-#         key_list = list(self.listes.keys())
-#         val_list = list(self.listes.values())
- 
-#         position = val_list.index(sort_lists[-1])
-        
-#         print("The flavour with bigger selling today was", key_list[position].capitalize(), "by selling: ", sort_lists[-1], "Scoops")
-#         return sort_lists
-
-#     def get_media(self):
-#         average = (float(mean(list(self.listes.values()))))
-#         sum_up = sum(list(self.listes.values())) 
-#         return print("From ", sum_up, "scoops sold of the", self.name, ",at least", average, "of the scoops were of this same flavour"   )
-
-#     def get_less_favorite(self):
-#         for i in self.listes:
-#             if self.listes[i] == 0:
-#                 self.listes[i] = float('NaN')
-#             else: 
-#                 pass
-#         print(sum(map(isnan, list(self.listes.values()))), '/', len(self.listes), "flavours in", self.name, "didnt sold any scoop at all")
-
-
-#         clean = list(filterfalse(isnan, list(self.listes.values())))
-#         key_list = list(self.listes.keys())
-#         val_list = list(self.listes.values())
-#         print("The next flavours were the only ones that made sales today")
-#         for i in clean: 
-#             position = val_list.index(i)
-#             print("- ", key_list[position].capitalize()) 
-
 def check_sells(list):
     x = 0
     for flavs in list:
---
+++ test_scooper.py
@@ -0,0 +1,20 @@
+sold=[]
+def make_bill(scoops, bill = 0):
+    if scoops == 1:
+        bill += 5
+        print("Your bill is: ", bill)
+        sold.append(bill)
+    else:
+        bill += 7
+        print("Your bill is: ", bill)
+        sold.append(bill)
+
+make_bill(1)
+make_bill(2)
+
+def test_makeBill():
+    assert sum(sold) == 12
+
+
+
+
